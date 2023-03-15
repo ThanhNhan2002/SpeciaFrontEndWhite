f508c277958cfe3c7f55dbf7be83426207611eb5
ThanhNhanNguyen <thanhnhan.b12002@gmail.com>
2023-03-15 22:34:54 +1100

    add svg file for checked button

--- src/pages/Marketplace/WorkerDetails.js
+++ src/pages/Marketplace/WorkerDetails.js
@@ -11,6 +11,8 @@ import { useParams } from 'react-router-dom';
 import Description from '../DigitalWorkers/Description';
 import InitialParameters from '../DigitalWorkers/InitialParameters';
 import Capabilities from './Capabilities';
+
+import PurchaseResultModal from './PurchaseResultModal';
 // import RunHistory from './RunHistory';
 // import RuntimeParameterModal from './RuntimeParameterModal';
 // import BotExecutingModal from './BotExecutingModal';
@@ -20,18 +22,19 @@ export default() =>  {
 
     let { digitalWorkerId } = useParams();
 
-    // const [runtimeParameterModalShow, setRuntimeParameterModalShow] = useState(false)
+    const [purchaseResult, setPurchaseResult] = useState(false)
 
     // const [botExecutingModalShow, setBotExecutingModalShow] = useState(false)
 
     // const [resultModalShow, setResultModalShow] = useState(false)
 
-    // function openRuntimeParameterModal(){
-    //     setRuntimeParameterModalShow(true)
-    // }
-    // function closeRuntimeParameterModal(){
-    //     setRuntimeParameterModalShow(false)
-    // }
+    function openPurchaseResult(){
+        setPurchaseResult(true)
+    }
+
+    function closePurchaseResult(){
+        setPurchaseResult(false)
+    }
 
     // function openBotExecutingModal(){
     //     setRuntimeParameterModalShow(false)
@@ -57,6 +60,8 @@ export default() =>  {
     return (
         <div style={{ display: 'flex', textAlign: "left", height: '100%', color: 'rgb(70, 70, 70)'}}>
             <div style={{ flex: 1, backgroundColor: 'white', marginRight: '10px', boxShadow: '0px 0px 10px rgb(100, 100, 100, 0.1)', borderRadius: '12px', border: '1px solid rgb(230, 230, 230)', marginTop: "20px", marginBottom: '20px'}}>
+            {/* <div style={{display: 'flex', flexDirection:'column', justifyContent:'center', paddingLeft: '20px', paddingRight: '20px', paddingBottom: '20px'}}> */}
+                
                 <Link to="/marketplace/" style={{textDecoration: 'none', color: 'black'}}>
                     <button className='btn-close' style={{"fontSize": '1.2rem', marginTop: '20px', marginLeft: '20px'}}></button>
                 </Link>
@@ -67,8 +72,8 @@ export default() =>  {
                         <p style={{fontSize: '1.1rem', fontWeight: '400', color: 'rgb(150, 150, 150)'}}>Spectar Group</p>
                     </div>
                     <div style={{display: 'flex', flexDirection:'column', height: '80px', justifyContent:'space-between', alignItems:'center' }}>
-                      <Button onClick={() => 'doNothing'} style={{paddingRight: '20px', paddingLeft: '20px', width: '200px'}} variant="secondary">Free Trial</Button>
-                      <Button onClick={() => 'doNothing'} style={{paddingRight: '20px', paddingLeft: '20px', width: '200px'}} variant="secondary">$19.99</Button>
+                      <Button onClick={() => setPurchaseResult(true)} style={{paddingRight: '20px', paddingLeft: '20px', width: '200px'}} variant="secondary">Free Trial</Button>
+                      <Button onClick={() => setPurchaseResult(true)} style={{paddingRight: '20px', paddingLeft: '20px', width: '200px'}} variant="secondary">$19.99</Button>
                     </div>
                 </div>
             </div>
@@ -82,6 +87,8 @@ export default() =>  {
                 <RunHistory/> */}
             </div>
 
+            
+            <PurchaseResultModal isShow={purchaseResult}  onClose={closePurchaseResult} ></PurchaseResultModal>
             {/* <RuntimeParameterModal isShow={runtimeParameterModalShow} onClose={closeRuntimeParameterModal} onRun={openBotExecutingModal}/>
 
             <BotExecutingModal isShow={botExecutingModalShow} onClose={closeBotExecutingModal}/> */}
