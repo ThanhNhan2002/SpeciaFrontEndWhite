45b4a8f66deed50154144ba3766710f2f2364f6e
ThanhNhanNguyen <thanhnhan.b12002@gmail.com>
2023-06-28 12:53:44 +1000

    fix the the checked icon after running a task successfully, enable the writing box in the home page

--- src/pages/DigitalWorkers/ResultModal.js
+++ src/pages/DigitalWorkers/ResultModal.js
@@ -4,16 +4,17 @@ import Form from 'react-bootstrap/Form';
 
 import Button from 'react-bootstrap/Button';
 import React, { Component } from 'react';
+import checkIcon from '../../resources/checkIcon.svg'
 
 export default (props) => {
     return (
         <>
             <Modal show={props.isShow} onHide={props.onClose} centered>
                 <Modal.Body>
-                    <div style={{paddingLeft: '30px', paddingRight: '30px', paddingBottom: '20px', paddingTop: '20px'}}>
-                        <p style={{fontSize: '2rem'}}>Task sucessfully completed!</p>
-                        <div style={{textAlign: 'center', padding: '50px 0'}}>
-                            
+                    <div style={{paddingLeft: '30px', paddingRight: '30px', paddingBottom: '20px', paddingTop: '20px', display: 'flex', flexDirection:'column', alignItems: 'center'}}>
+                        <p style={{fontSize: '2rem'}}>Task successfully completed!</p>
+                        <div><img src={checkIcon} style={{ height: 100, width: 100, fontWeight:'lighter'}} alt="checked icon" /></div>
+                        <div style={{textAlign: 'center', padding: '10px 0'}}>
                         </div>
                         <div style={{textAlign: 'center'}}>
                             <Button style={{paddingLeft: '20px', paddingRight: '20px'}} variant="secondary" onClick={props.onRun}>
